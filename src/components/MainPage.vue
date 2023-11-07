d36acd346613ae8b6b19a25ed8f25e41e166d0cd
yanlong-sun <sunyanlong712@gmail.com>
2023-11-08 00:27:50 +0800

    hoempage

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -3,8 +3,13 @@
     <div class="items">
       <div class="headLine">{{ $t('joinTheBand') }}</div>
       <button class="btn-allJobs">{{ $t('exploreAllJobs') }}</button>
-      <div class="searchBar">
-        <input class="inputBox" type="text" />
+      <div class="searchBox">
+        <img
+          class="searchIcon"
+          src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTkiIGhlaWdodD0iMjEiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTE0LjM4NyAxNC42MjNBOC40NyA4LjQ3IDAgMDAxNyA4LjUgOC41IDguNSAwIDEwOC41IDE3YTguNDU0IDguNDU0IDAgMDA1LjEyNS0xLjczbDQuNDAxIDUuMTUzLjc2LS42NDktNC4zOTktNS4xNTF6TTguNSAxNkM0LjM2NCAxNiAxIDEyLjYzNiAxIDguNVM0LjM2NCAxIDguNSAxIDE2IDQuMzY0IDE2IDguNSAxMi42MzYgMTYgOC41IDE2eiIgZmlsbD0iIzAwMCIvPjwvc3ZnPg=="
+        />
+        <input class="inputBox" type="text" :placeholder="$t('searchJobs')" />
+        <button class="btn-search">&rarr;</button>
       </div>
     </div>
   </div>
@@ -22,18 +27,17 @@
   .items {
     display: flex;
     flex-direction: column;
+    width: 90%;
     position: absolute;
     top: 30%;
-    left: 30px;
-    top .headLine {
+    left: 5%;
+    .headLine {
       font-size: 72px;
       font-weight: bold;
+      margin-bottom: 28px;
       color: #ffffff;
-      flex: 3;
-      text-justify: end;
     }
     .btn-allJobs {
-      flex: 3;
       height: 72px;
       width: 220px;
       border: none;
@@ -49,9 +53,50 @@
       transition: all ease 0.2s;
       color: #191414;
     }
-    .searchBar {
-      flex: 3;
-      height: 64px;
+    .searchBox {
+      width: 95%;
+      position: relative;
+      .searchIcon {
+        position: absolute;
+        width: 24px;
+        height: 24px;
+        left: 22px;
+        bottom: 20px;
+        color: #ffffff;
+      }
+
+      .inputBox {
+        margin-top: 10%;
+        width: 100%;
+        height: 64px;
+        border-radius: 40px;
+        border: none;
+        background-color: #ff4632;
+        font-size: 22px;
+        padding-left: 64px;
+      }
+      ::placeholder {
+        color: #ffffff;
+      }
+      .inputBox:focus {
+        outline-width: 0;
+        background-color: #ffffff;
+        outline: 1px solid #191414;
+        .btn-search {
+          border: 1px solid #191414;
+        }
+      }
+      .btn-search {
+        position: absolute;
+        right: -55px;
+        bottom: 6px;
+        height: 58px;
+        width: 58px;
+        border-radius: 50%;
+        border: none;
+        font-size: 28px;
+        background-color: #ffffff;
+      }
     }
   }
 }
